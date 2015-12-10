b9db0cf3cef2931c5c577f2dab06705c148e40f2
ihaveamac <ihaveamac@icloud.com>
2015-12-10 08:07:22 -0800

    support NTR CFW screenshots location, remove .3dsx, other changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # screenshot-merge
 This tool is for really lazy people who want to merge their ninjhax 2.5 screenshots on their 3DS. Inspired by [RedInquisitive's PC Screenshot Tool](https://github.com/RedInquisitive/Screenshot-Tool).
 
-This uses lpp-3ds r4 by Rinnegatamante. Please see [its repo](https://github.com/Rinnegatamante/lpp-3ds) for more details, including license. The "normal" build with error handling is in this repository, releases use the "unsafe" build with no error handling.
+This uses lpp-3ds r4 by Rinnegatamante. Please see [its repo](https://github.com/Rinnegatamante/lpp-3ds) for more details, including license. Releases use the "unsafe" build with no error handling.
 
 It searches `/screenshots` for files that match the name `scr_X_TOP_LEFT.png` and `scr_X_BOTTOM.png`, puts them together, and saves the result at `/screenshots-merged/scr_X_MERGED.bmp`.
 
--- index.lua
+++ index.lua
@@ -1,18 +1,23 @@
 --ihaveamac--
 -- https://github.com/ihaveamac/screenshot-merge
 -- licensed under the MIT license - see https://github.com/ihaveamac/screenshot-merge/blob/master/LICENSE.md
-version = "1.0"
+version = "1.1 NTR CFW testing"
 
 -- input_folder exists for debugging reasons, and the fact that I back up my screenshots
 input_folder = "screenshots"
 output_folder = "screenshots-merged"
 
+NINJHAX = "ninjhax"
+NTR     = "ntr"
+
 c_white = Color.new(255, 255, 255)
-c_grey = Color.new(127, 127, 127)
+c_grey  = Color.new(127, 127, 127)
 c_black = Color.new(0, 0, 0)
 System.setCpuSpeed(NEW_3DS_CLOCK)
 System.createDirectory("/"..input_folder) -- to prevent errors
 System.createDirectory("/"..output_folder)
+System.createDirectory("/"..output_folder.."/"..NINJHAX)
+System.createDirectory("/"..output_folder.."/"..NTR)
 
 -- this doesn't have a real purpose other than to switch "SD" and "microSD"
 model = System.getModel()
@@ -32,11 +37,11 @@ function print(x, y, text)
     Screen.debugPrint(x, y, text, c_white, TOP_SCREEN)
 end
 -- 'cause double buffering stuff
-function doubleDraw(func)
-    func()
+function doubleDraw(draw)
+    draw()
     Screen.flip()
     Screen.refresh()
-    func()
+    draw()
 end
 function drawMain()
     Screen.waitVblankStart()
@@ -50,20 +55,18 @@ end
 
 doubleDraw(function()
     drawMain()
-    print(5, 25, "This tool will merge all screenshots in the")
-    print(5, 40, "\""..input_folder.."\" folder on your 3DS's ")
-    print(5, 55, sdcardtype.." card.")
-    print(5, 75, "The resulting images will be placed in a")
-    print(5, 90, "folder called \""..output_folder.."\".")
-    print(5, 110, "If this folder exists, you should back it")
-    print(5, 125, "up to prevent conflicts.")
-    print(5, 145, "Only those with scr_X_TOP_LEFT.png and")
-    print(5, 160, "scr_X_BOTTOM.png will be merged.")
-    print(5, 200, "A: next")
-    print(5, 215, "B: exit")
+    print(5, 25, "This tool will merge screenshots on your")
+    print(5, 40, "3DS's "..sdcardtype.." card.")
+    print(5, 60, "The resulting images will be placed in a")
+    print(5, 75, "folder called \""..output_folder.."\".")
+    print(5, 95, "If this folder exists, you should back it")
+    print(5, 110, "up to prevent conflicts.")
+    print(5, 130, "Only those with a top and bottom image")
+    print(5, 145, "will be merged.")
+    print(5, 185, "A: next")
+    print(5, 200, "B: exit")
 end)
 repeat
-    --noinspection GlobalCreationOutsideO
     pad = Controls.read()
     if Controls.check(pad, KEY_B) then
         System.exit()
@@ -73,39 +76,58 @@ until Controls.check(pad, KEY_A)
 doubleDraw(function()
     drawMain()
     print(5, 25, "The resulting screenshots will be in bmp")
-    print(5, 40, "format with the filename scr_X_MERGED.bmp.")
+    print(5, 40, "format in the \""..output_folder.."\".")
     print(5, 60, "Getting list of files to merge...")
 end)
 -- for seeing if a number is already seen
-numbers = {}
+numbers = {[NINJHAX] = {}, [NTR] = {}}
 -- for the actual numbers, checking if TOP_LEFT and BOTTOM exist
 files_to_process = {}
+
+overwriting_files = false
+-- NINJHAX
 for _, v in pairs(System.listDirectory("/"..input_folder)) do
     local num = v.name:sub(5, v.name:find("_", 5) - 1)
-    if not numbers[num] then
-        numbers[num] = true
+    if not numbers[NINJHAX][num] then
+        numbers[NINJHAX][num] = true
     end
 end
-overwriting_files = false
-for k, _ in pairs(numbers) do
+for k, _ in pairs(numbers[NINJHAX]) do
     if System.doesFileExist("/"..input_folder.."/scr_"..k.."_TOP_LEFT.png") and System.doesFileExist("/"..input_folder.."/scr_"..k.."_BOTTOM.png") then
-        table.insert(files_to_process, k)
-        if System.doesFileExist("/"..output_folder.."/scr_"..k.."_MERGED.bmp") then
+        table.insert(files_to_process, {k, NINJHAX})
+        if System.doesFileExist("/"..output_folder.."/"..NINJHAX.."/scr_"..k.."_MERGED.bmp") then
             overwriting_files = true
         end
     end
 end
+
+-- NTR
+for _, v in pairs(System.listDirectory("/")) do
+    local num = v.name:sub(5, 8)
+    if not numbers[NTR][num] then
+        numbers[NTR][num] = true
+    end
+end
+for k, _ in pairs(numbers[NTR]) do
+    if System.doesFileExist("/top_"..k..".bmp") and System.doesFileExist("/bot_"..k..".bmp") then
+        table.insert(files_to_process, {k, NTR})
+        if System.doesFileExist("/"..output_folder.."/"..NTR.."/mrg_"..k..".bmp") then
+            overwriting_files = true
+        end
+    end
+end
+
 doubleDraw(function()
     drawMain()
     print(5, 25, "The resulting screenshots will be in bmp")
-    print(5, 40, "format with the filename scr_X_MERGED.bmp.")
+    print(5, 40, "format in the \""..output_folder.."\".")
     print(5, 60, "Processing "..(#files_to_process * 2).." screenshots, ")
     print(5, 75, "creating a total of "..#files_to_process.." images.")
     if overwriting_files then
         -- this will be split into multiple lines
         print(5, 95, "WARNING: It has been detected that some")
         print(5, 110, "files will be overwritten in the process.")
-        print(5, 130, "It is highly recommended to move or the")
+        print(5, 130, "It is highly recommended to move the")
         print(5, 145, "\""..output_folder.."\" folder before")
         print(5, 160, "starting.")
         print(5, 200, "X: start")
@@ -116,7 +138,6 @@ doubleDraw(function()
     end
 end)
 repeat
-    --noinspection GlobalCreationOutsideO
     pad = Controls.read()
     if Controls.check(pad, KEY_B) then
         System.exit()
@@ -136,14 +157,25 @@ doubleDraw(function()
 end)
 stop_count = 0
 for i = 1, #files_to_process do
-    System.deleteFile("/"..output_folder.."/scr_"..files_to_process[i].."_MERGED.bmp")
-    local top = Screen.loadImage("/"..input_folder.."/scr_"..files_to_process[i].."_TOP_LEFT.png")
-    Screen.drawImage(0, 0, top, TOP_SCREEN)
-    Screen.freeImage(top)
-    local bottom = Screen.loadImage("/"..input_folder.."/scr_"..files_to_process[i].."_BOTTOM.png")
-    Screen.drawImage(0, 0, bottom, BOTTOM_SCREEN)
-    Screen.freeImage(bottom)
-    System.takeScreenshot("/"..output_folder.."/scr_"..files_to_process[i].."_MERGED.bmp", false)
+    if files_to_process[i][2] == NINJHAX then
+        System.deleteFile("/"..output_folder.."/"..NINJHAX.."/scr_"..files_to_process[i][1].."_MERGED.bmp")
+        local top = Screen.loadImage("/"..input_folder.."/scr_"..files_to_process[i][1].."_TOP_LEFT.png")
+        Screen.drawImage(0, 0, top, TOP_SCREEN)
+        Screen.freeImage(top)
+        local bottom = Screen.loadImage("/"..input_folder.."/scr_"..files_to_process[i][1].."_BOTTOM.png")
+        Screen.drawImage(0, 0, bottom, BOTTOM_SCREEN)
+        Screen.freeImage(bottom)
+        System.takeScreenshot("/"..output_folder.."/"..NINJHAX.."/scr_"..files_to_process[i][1].."_MERGED.bmp", false)
+    elseif files_to_process[i][2] == NTR then
+        System.deleteFile("/"..output_folder.."/"..NTR.."/mrg_"..files_to_process[i][1]..".bmp")
+        local top = Screen.loadImage("/top_"..files_to_process[i][1]..".bmp")
+        Screen.drawImage(0, 0, top, TOP_SCREEN)
+        Screen.freeImage(top)
+        local bottom = Screen.loadImage("/bot_"..files_to_process[i][1]..".bmp")
+        Screen.drawImage(0, 0, bottom, BOTTOM_SCREEN)
+        Screen.freeImage(bottom)
+        System.takeScreenshot("/"..output_folder.."/"..NTR.."/mrg_"..files_to_process[i][1]..".bmp", false)
+    end
     doubleDraw(function()
         drawMain()
         print(5, 25, "Processing screenshots...")
@@ -154,7 +186,6 @@ for i = 1, #files_to_process do
         print(190, 45, "%")
         print(5, 60, "Hold B to stop.")
     end)
-    --noinspection GlobalCreationOutsideO
     stop_count = i
     if Controls.check(Controls.read(), KEY_B) then
         break
@@ -174,9 +205,13 @@ doubleDraw(function()
         print(5, 60, "Done!")
     end
     print(5, 100, "A: exit")
+    if System.doesFileExist("/gridlauncher/glinfo.txt") then
+        print(5, 120, "If you are using mashers's grid launcher,")
+        print(5, 135, "you should use the exit shortcut instead.")
+        print(5, 155, "ninjhax 2.5 exit shortcut: L+R+Down+B")
+    end
 end)
 while true do
-    --noinspection GlobalCreationOutsideO
     pad = Controls.read()
     if Controls.check(pad, KEY_A) then
         System.exit()
