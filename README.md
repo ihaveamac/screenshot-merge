48665128fb941d0273d96021afc16288412d96f9
ihaveamac <ihaveamac@icloud.com>
2015-12-09 16:23:27 -0800

    actual screenshot merging by taking screenshots itself (really should look into something better)

---
+++ README.md
@@ -0,0 +1,5 @@
+# screenshot-merge
+Stuff will go here...
+
+# License
+screenshot-merge is under the MIT license. Lua Player Plus is under the GPLv3 license.
--- index.lua
+++ index.lua
@@ -3,9 +3,15 @@
 -- licensed under the MIT license - see https://github.com/ihaveamac/screenshot-merge/blob/master/LICENSE.md
 version = "1.0"
 
+-- input_folder exists for debugging reasons, and the fact that I back up my screenshots
+input_folder = "screenshots-old"
+output_folder = "screenshots-merged"
+
 c_white = Color.new(255, 255, 255)
-System.createDirectory("/screenshots") -- to prevent errors
-System.createDirectory("/screenshots-merged")
+c_black = Color.new(0, 0, 0)
+System.setCpuSpeed(NEW_3DS_CLOCK)
+System.createDirectory("/"..input_folder) -- to prevent errors
+System.createDirectory("/"..output_folder)
 
 -- this doesn't have a real purpose other than to switch "SD" and "microSD"
 model = System.getModel()
@@ -43,10 +49,10 @@ end
 doubleDraw(function()
     drawMain()
     print(5, 25, "This tool will merge all screenshots in the")
-    print(5, 40, "\"screenshots\" folder on your 3DS's ")
+    print(5, 40, "\""..input_folder.."\" folder on your 3DS's ")
     print(5, 55, sdcardtype.." card.")
     print(5, 75, "The resulting images will be placed in a")
-    print(5, 90, "folder called \"screenshots-merged\".")
+    print(5, 90, "folder called \""..output_folder.."\".")
     print(5, 110, "If this folder exists, you should back it")
     print(5, 125, "up to prevent conflicts.")
     print(5, 145, "Only those with scr_X_TOP_LEFT.png and")
@@ -72,7 +78,7 @@ end)
 numbers = {}
 -- for the actual numbers, checking if TOP_LEFT and BOTTOM exist
 files_to_process = {}
-for _, v in pairs(System.listDirectory("/screenshots")) do
+for _, v in pairs(System.listDirectory("/"..input_folder)) do
     local num = v.name:sub(5, v.name:find("_", 5) - 1)
     if not numbers[num] then
         numbers[num] = true
@@ -80,24 +86,31 @@ for _, v in pairs(System.listDirectory("/screenshots")) do
 end
 overwriting_files = false
 for k, _ in pairs(numbers) do
-    if System.doesFileExist("/screenshots/scr_"..k.."_TOP_LEFT.png") and System.doesFileExist("/screenshots/scr_"..k.."_BOTTOM.png") then
+    if System.doesFileExist("/"..input_folder.."/scr_"..k.."_TOP_LEFT.png") and System.doesFileExist("/"..input_folder.."/scr_"..k.."_BOTTOM.png") then
         table.insert(files_to_process, k)
-        if System.doesFileExist("/screenshots-merged/scr_"..k.."_MERGED.bmp") then
+        if System.doesFileExist("/"..output_folder.."/scr_"..k.."_MERGED.bmp") then
             overwriting_files = true
         end
     end
 end
-overwriting_files = true
 doubleDraw(function()
     drawMain()
     print(5, 25, "The resulting screenshots will be in bmp")
     print(5, 40, "format with the filename scr_X_MERGED.bmp.")
-    print(5, 60, "Processing a total of "..(#files_to_process * 2).." screenshots, ")
-    print(5, 75, "creating "..#files_to_process.." images.")
+    print(5, 60, "Processing "..(#files_to_process * 2).." screenshots, ")
+    print(5, 75, "creating a total of "..#files_to_process.." images.")
     if overwriting_files then
         -- this will be split into multiple lines
-        print(5, 75, "WARNING: It has been detected that some files will be overwritten in the process. It is highly recommended to move or the \"screenshots-merged\" folder before starting.")
+        print(5, 95, "WARNING: It has been detected that some")
+        print(5, 110, "files will be overwritten in the process.")
+        print(5, 130, "It is highly recommended to move or the")
+        print(5, 145, "\""..output_folder.."\" folder before")
+        print(5, 160, "starting.")
+        print(5, 200, "X: start")
+        print(5, 215, "B: exit")
     end
+    print(5, 115, "X: start")
+    print(5, 130, "B: exit")
 end)
 repeat
     --noinspection GlobalCreationOutsideO
@@ -106,3 +119,51 @@ repeat
         System.exit()
     end
 until Controls.check(pad, KEY_X)
+
+-- show 0% before files are actually processed
+doubleDraw(function()
+    drawMain()
+    print(5, 25, "Processing screenshots...")
+    print(5, 45, "0")
+    print(51, 45, "/")
+    print(73, 45, #files_to_process)
+    print(160, 45, "0")
+    print(190, 45, "%")
+end)
+for i = 1, #files_to_process do
+    System.deleteFile("/"..output_folder.."/scr_"..files_to_process[i].."_MERGED.bmp")
+    local top = Screen.loadImage("/"..input_folder.."/scr_"..files_to_process[i].."_TOP_LEFT.png")
+    Screen.drawImage(0, 0, top, TOP_SCREEN)
+    Screen.freeImage(top)
+    local bottom = Screen.loadImage("/"..input_folder.."/scr_"..files_to_process[i].."_BOTTOM.png")
+    Screen.drawImage(0, 0, bottom, BOTTOM_SCREEN)
+    Screen.freeImage(bottom)
+    System.takeScreenshot("/"..output_folder.."/scr_"..files_to_process[i].."_MERGED.bmp", false)
+    doubleDraw(function()
+        drawMain()
+        print(5, 25, "Processing screenshots...")
+        print(5, 45, i)
+        print(51, 45, "/")
+        print(73, 45, #files_to_process)
+        print(160, 45, math.floor((i / #files_to_process) * 100))
+        print(190, 45, "%")
+    end)
+end
+doubleDraw(function()
+    drawMain()
+    print(5, 25, "Processing screenshots...")
+    print(5, 45, #files_to_process)
+    print(51, 45, "/")
+    print(73, 45, #files_to_process)
+    print(160, 45, "100")
+    print(190, 45, "%")
+    print(5, 60, "Done!")
+    print(5, 100, "A/B: exit")
+end)
+while true do
+    --noinspection GlobalCreationOutsideO
+    pad = Controls.read()
+    if Controls.check(pad, KEY_A) or Controls.check(pad, KEY_B) then
+        System.exit()
+    end
+end
