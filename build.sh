126d40d695c2ab13b72e00d0efedf96036d199d2
ihaveamac <ian@ianburgwin.net>
2017-06-20 06:36:40 -0700

    commit build.sh changes from over a year ago, rsf changes

--- build.sh
+++ build.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
-TITLEVER=1056
+TITLEVER=1088  # how to use: https://www.3dbrew.org/wiki/Title_list#Versions
 VERSION="1.2"
+HOMEBREWNAME="screenshot merge"
+HOMEBREWFILENAME="screenshot-merge"
+CIADESCRIPTION="Merge top and botton screenshots into single images"
+PUBLISHER="ihaveamac"
+
 rm -r output/
 rm -r tmp-output/
 mkdir output/
 mkdir tmp-output/
-bannertool makesmdh -s "screenshot merge" -l "screenshot merge" -p "ihaveamac" -i resources/icon.png -f visible,nosavebackups -o tmp-output/icon.bin
+bannertool makesmdh -s "$HOMEBREWNAME" -l "$HOMEBREWNAME" -p "$PUBLISHER" -i resources/icon.png -f visible,nosavebackups -o tmp-output/icon.bin
 bannertool makebanner -i resources/banner.png -ca resources/hbchannel.cwav -o tmp-output/banner.bin
-smdhtool --create "screenshot merge $VERSION" "Merge top and botton screenshots into single images" "ihaveamac" resources/icon.png output/screenshot-merge.smdh
+smdhtool --create "$HOMEBREWNAME $VERSION" "$CIADESCRIPTION" "$PUBLISHER" resources/icon.png "tmp-output/${HOMEBREWFILENAME}.smdh"
 3dstool -cvtf romfs tmp-output/romfs.bin --romfs-dir romfs/
 if [[ "$1" == "release" ]]; then
     echo "release"
-    makerom -f cia -o output/screenshot-merge${VERSION}.cia -elf resources/lpp-3ds-unsafe.elf -rsf resources/cia_workaround.rsf -icon tmp-output/icon.bin -banner tmp-output/banner.bin -exefslogo -target t -romfs tmp-output/romfs.bin -ver $TITLEVER
-    3dsxtool resources/lpp-3ds-unsafe.elf output/screenshot-merge.3dsx --smdh=output/screenshot-merge.smdh --romfs=romfs/
+    makerom -f cia -o "output/${HOMEBREWNAME}${VERSION}.cia" -elf resources/lpp-3ds-unsafe.elf -rsf resources/cia_workaround.rsf -icon tmp-output/icon.bin -banner tmp-output/banner.bin -exefslogo -target t -romfs tmp-output/romfs.bin -ver $TITLEVER
+    3dsxtool resources/lpp-3ds-unsafe.elf "output/${HOMEBREWNAME}.3dsx" --smdh="tmp-output/${HOMEBREWNAME}.smdh" --romfs=romfs/
     mkdir release/
-    rm release/screenshot-merge${VERSION}.zip
+    rm "release/${HOMEBREWNAME}${VERSION}.zip"
     cd output/
-    zip ../release/screenshot-merge${VERSION}.zip screenshot-merge.3dsx screenshot-merge.smdh
-    cp screenshot-merge${VERSION}.cia ../release
+    zip "../release/${HOMEBREWFILENAME}${VERSION}.zip" "${HOMEBREWFILENAME}.3dsx" "${HOMEBREWFILENAME}.smdh"
+    cp "${HOMEBREWFILENAME}${VERSION}.cia" ../release
 else
     echo "testing"
-    makerom -f cia -o output/screenshot-merge${VERSION}.cia -elf resources/lpp-3ds-normal.elf -rsf resources/cia_workaround.rsf -icon tmp-output/icon.bin -banner tmp-output/banner.bin -exefslogo -target t -romfs tmp-output/romfs.bin -ver $TITLEVER
-    3dsxtool resources/lpp-3ds-normal.elf output/screenshot-merge.3dsx --smdh=output/screenshot-merge.smdh --romfs=romfs/
+    makerom -f cia -o "output/${HOMEBREWFILENAME}${VERSION}.cia" -elf resources/lpp-3ds-normal.elf -rsf resources/cia_workaround.rsf -icon tmp-output/icon.bin -banner tmp-output/banner.bin -exefslogo -target t -romfs tmp-output/romfs.bin -ver $TITLEVER
+    3dsxtool resources/lpp-3ds-normal.elf "output/${HOMEBREWFILENAME}.3dsx" --smdh="tmp-output/${HOMEBREWFILENAME}.smdh" --romfs=romfs/
 fi
